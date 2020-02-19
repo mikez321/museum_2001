c3b2376559fe0e5e7ad344fb360eead3a3e4a1e1
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-19 09:05:52 -0700

    Build first test

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -1,6 +1,15 @@
 require './lib/exhibit'
+require './lib/patron'
 require 'minitest/autorun'
 require 'minitest/pride'
 
 class ExhibitTest < Minitest::Test
+
+  def setup
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  def test_it_exists
+    assert_instance_of Exhibit, @exhibit
+  end
 end
