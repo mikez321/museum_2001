c890ff5f4ea5ff3e8a986610ad7189944a34a63c
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-19 10:39:39 -0700

    Create test for persons by exhibits

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -54,4 +54,21 @@ class MuseumTest < Minitest::Test
     @dmns.admit(@patron_3)
     assert_equal [@patron_1, @patron_2, @patron_3], @dmns.patrons
   end
+
+  def test_it_can_find_patrons_by_interests
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_2.add_interest("Dead Sea Scrolls")
+    @patron_3.add_interest("Dead Sea Scrolls")
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+
+    expected = {
+      @gems_and_minerals => @patron_1,
+      @dead_sea_scrolls => @patron_1, @patron_2, @patron3
+      }
+
+    assert_equal expected, dmns.patrons_by_exhibit_interest
+  end
 end
