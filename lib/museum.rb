ef9aab23d1c7c781d282bdc2c7f14dd59a0f3cc2
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-19 10:22:15 -0700

    Create method to match recommended exhibits with interests

--- lib/museum.rb
+++ lib/museum.rb
@@ -8,4 +8,21 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    names = exhibits.map do |exhibit|
+      exhibit.name
+    end
+    match = patron.interests & names
+    recommended = []
+    exhibits.each do |exhibit|
+      match.each do |name|
+        if name == exhibit.name
+          recommended << exhibit
+        end
+      end
+    end
+    recommended.reverse
+  end
+
 end
