0b0f4eee5f7028233526e449c6127c83280fb5ce
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-19 09:26:01 -0700

    Create exist test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -5,4 +5,12 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class MuseumTest < Minitest::Test
+
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+  end
+
+  def test_it_exists
+    assert_instance_of Museum, @dmns
+  end
 end
